a6e62445f0caab44c132b37ffef04e44591125da
AjoCool <ajo.sjolin@gmail.com>
2026-04-20 09:24:18 +0200

    update

--- IS/IS.ino
+++ IS/IS.ino
@@ -2,25 +2,26 @@
 #include <WiFi.h>
 #include <Adafruit_NeoPixel.h>
 
-const int LED_PIN 5;
-const int NUMPIXELS 64,
+const int LED_PIN = 5;
+const int NUMPIXELS = 9;
 
 Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-const char* ssid = "Ali";
-const char* password = "123456789";
-const char* serverIP = "10.224.33.150"; // DIN DATORS IP
-const int serverPort = 8000;
+const char* ssid = "Gabriel - IPhone (2)";
+const char* password = "ottmar123";
+const char* serverIP = "172.20.10.8"; // DIN DATORS IP
+const int serverPort = 8500;
 
 WiFiClient client;
-unsigned long lastSyncTime = 0;
+
+const unsigned long updateInterval = 200;
+unsigned long lastUpdate = 0;
 
 const int rowPins[3] = {15, 17, 19};
 const int colPins[3] = {22, 23, 2};
-bool lastButtonState[64] = {false};
+bool lastButtonState[9] = {false};
+bool started = true;
 
-// NYTT: Variabel för att minnas om vi redan har gjort segerdansen
-bool hasCelebrated = false; 
 
 void setup() {
   Serial.begin(115200);
@@ -37,88 +38,74 @@ void setup() {
   Serial.println("Ansluten till WiFi!");
 }
 
-void sendClick(int index) {
+void sendClick(String message) {
   if (client.connect(serverIP, serverPort)) {
-    client.print("CLICK:");
-    client.println(index);
+    client.println(message);
+    client.readStringUntil('\n');
     client.stop();
-    lastSyncTime = 0; // Tvinga omedelbar uppdatering
   }
 }
 
-// --- NY FUNKTION: SEGERDANSEN (FYRVERKERI) ---
-void winDance() {
-  // En lista med 6 starka färger (Röd, Grön, Blå, Gul, Lila, Cyan)
-  uint32_t colors[] = {
-    pixels.Color(255, 0, 0), pixels.Color(0, 255, 0), pixels.Color(0, 0, 255), 
-    pixels.Color(255, 255, 0), pixels.Color(255, 0, 255), pixels.Color(0, 255, 255)
-  };
-  
-  // Dansa i 15 snabba pulser
-  for(int varv = 0; varv < 15; varv++) {
-    for(int i = 0; i < NUMPIXELS; i++) {
-      // Välj en slumpmässig färg (0 till 5) för varje diod
-      pixels.setPixelColor(i, colors[random(0, 6)]); 
+void startShow(){
+  for (int i; i > 5; i++){
+    for (int j; j > 9; j++){
+      pixels.setPixelColor(j, pixels.Color(random(255), random(255), random(255)));
     }
     pixels.show();
-    delay(80); // Vänta 80 millisekunder (ger en snabb, blinkande effekt)
+    delay(1000);
   }
+  started = false;
 }
-// ---------------------------------------------
+
+  
 
 void loop() {
+  if (started){
+    startShow();
+  }
+  
   if(WiFi.status() != WL_CONNECTED) return;
 
-  // 1. Scanna knappar
-  for(int r = 0; r < 8; r++) {
+  for(int r = 0; r < 3; r++) {
     digitalWrite(rowPins[r], LOW);
-    for(int c = 0; c < 8; c++) {
-      int index = r * 8 + c;
+    for(int c = 0; c < 3; c++) {
+      int index = r * 3 + c;
+      String button = String(r) + ":" + String(c);
       bool isPressed = (digitalRead(colPins[c]) == LOW);
 
       if(isPressed && !lastButtonState[index]) {
-        sendClick(index); 
+        sendClick(button);
       }
       lastButtonState[index] = isPressed;
     }
     digitalWrite(rowPins[r], HIGH);
   }
 
-  // 2. Synka med servern
-  if (millis() - lastSyncTime > 100) {
+  if (millis() - lastUpdate > updateInterval) {
+    lastUpdate = millis();
+
     if (client.connect(serverIP, serverPort)) {
-      client.println("SYNC");
+      client.println("update");
+
       unsigned long timeout = millis();
       while (client.available() == 0 && millis() - timeout < 2000) {}
 
       String response = client.readStringUntil('\n');
-      response.trim();
 
-      if (response.startsWith("BOARD:")) {
-        String board = response.substring(6); 
+      for (int i = 0; i < 9; i++) {
+        char p = response.charAt(i);
 
-        // --- NYTT: KOLLA OM NÅGON VANN ---
-        bool someoneWon = (board.indexOf('W') != -1); // Finns det ett 'W' på brädet?
-        if (someoneWon && !hasCelebrated) {
-          winDance(); // KÖR DANSEN!
-          hasCelebrated = true; // Kom ihåg att vi dansat så vi inte fastnar i en evig loop
-        } else if (!someoneWon) {
-          hasCelebrated = false; // Återställ minnet när Java-spelet rensar brädet ("Spela Igen")
+        if (p == 'R') {
+          pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+        } else if (p == 'B') {
+          pixels.setPixelColor(i, pixels.Color(0, 0, 255));
+        } else {
+          pixels.setPixelColor(i, pixels.Color(0, 0, 0));
         }
-        // ---------------------------------
-
-        // Rita sedan ut brädet som vanligt
-        for (int i = 0; i < 64; i++) {
-          char p = board.charAt(i);
-          if (p == 'R') pixels.setPixelColor(i, pixels.Color(255, 0, 0));
-          else if (p == 'B') pixels.setPixelColor(i, pixels.Color(0, 0, 255));
-          else if (p == 'W') pixels.setPixelColor(i, pixels.Color(0, 255, 0)); // Lyser fast grönt efter dansen
-          else pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-        }
-        pixels.show();
       }
+
+      pixels.show();
       client.stop();
     }
-    lastSyncTime = millis();
   }
 }
